c16e470a99782ee4bc32237e2608a731e3041bd0
pj-sullivan <patricia.sullivan199@gmail.com>
2025-10-28 10:39:33 -0400

    restructure analyses

--- analyses/01-cram-to-bam.sh
+++ analyses/01-cram-to-bam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Set up input file
-variants="variants/cosmic-germline-variants.tsv"
+variants="input/input.tsv"
 window=10000
 
 while read line; do
@@ -11,32 +11,34 @@ while read line; do
   gene=$(echo "$line" | cut -f 11)
   
   prefix="$KF_id-$gene-$chr-$coord_pos"
+  echo "Processing $prefix"
   
   ## TODO: Get window from splice event
-  coordinates="$chr:$(($coord_pos - $window"))-$(($coord_pos + $window))"
+  coordinates=$chr":"$(($coord_pos - $window))"-"$(($coord_pos + $window))
     
   ## get file id
-  crams=$(grep "$KF_id" manifest.tsv | grep "Aligned.out.sorted.cram" | grep -v "crai" | cut -f2)
+  crams=$(grep "$KF_id" input/manifest.tsv | grep "Aligned.out.sorted.cram" | grep -v "crai" | cut -f2)
     
-  ## loop through each CRAM
+  ## loop through each CRAM per patient
+  ## TODO: Make select from BS_ID an option?
   for cram in $crams; do
-    cram_path="cavatica/projects/sicklera/pbta-and-normal-crams/$cram"
+    cram_path="../data/cavatica/projects/sicklera/pbta-and-normal-crams/$cram"
     
-    # prefix: derive from file name (or override if you prefer)
+    # prefix: derive from file name
     prefix=$(basename "$cram" .Aligned.out.sorted.cram)
     
-    echo "Processing $cram_path"
-    bam_path="bams/${prefix}-${KF_id}-${gene}-${coordinates}.bam"
-    input_path="variants/${prefix}-${KF_id}-${gene}-${coordinates}.tsv"
+    echo "Converting $cram_path"
+    bam_path="results/bams/${prefix}-${KF_id}-${gene}-${coordinates}.bam"
+    # input_path="variants/${prefix}-${KF_id}-${gene}-${coordinates}.tsv"
       
     samtools view \
-      -T hg38.fa \
+      -T ../data/hg38.fa \
       -b \
       "$cram_path" \
       "$coordinates" \
-      -o "variants/${bam_path}"
+      -o "$bam_path"
     
-    samtools index "variants/${bam_path}"
+    samtools index "$bam_path"
     
   done
 done < <(tail -n +2 $variants)
