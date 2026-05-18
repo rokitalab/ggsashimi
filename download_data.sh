e2ef7582d1bdc1b4252d77451c14f6f1980b6523
rjcorb <rcorbett@childrensnational.org>
2026-05-18 12:55:29 -0400

    download refs from s3 bucket

--- download_data.sh
+++ download_data.sh
@@ -5,14 +5,19 @@ set -o pipefail
 
 # Download gencode v39 GTF
 cd refs
-GENCODE39="https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_39/gencode.v39.primary_assembly.annotation.gtf.gz"
+GENCODE39="https://bti-openaccess-us-east-1-prd-references.s3.us-east-1.amazonaws.com/gencode.v39.primary_assembly.annotation.gtf.gz"
 
 echo "Downloading ${GENCODE39##*/}"
-curl -L $GENCODE39 | gunzip -c > gencode.v39.primary_assembly.annotation.gtf
+curl -L $GENCODE39 > gencode.v39.primary_assembly.annotation.gtf
 
 # Download GRCh38 fasta file
-REFERENCE="https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.primary_assembly.genome.fa.gz"
+REFERENCE="https://bti-openaccess-us-east-1-prd-references.s3.us-east-1.amazonaws.com/GRCh38.primary_assembly.genome.fa"
 
 echo "Downloading ${REFERENCE##*/}"
-curl -L $REFERENCE | gunzip -c > GRCh38.primary_assembly.genome.fa
+curl -L $REFERENCE > GRCh38.primary_assembly.genome.fa
 
+# Download fasta index
+INDEX="https://bti-openaccess-us-east-1-prd-references.s3.us-east-1.amazonaws.com/GRCh38.primary_assembly.genome.fa.fai"
+
+echo "Downloading ${INDEX##*/}"
+curl -L $INDEX > GRCh38.primary_assembly.genome.fa.fai
